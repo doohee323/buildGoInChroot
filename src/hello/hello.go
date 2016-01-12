d36e0c224c58d19b134d780b5aac81a8c853f8dd
dewey hong <doohee323@gmail.com>
2016-01-12 13:43:07 -0800

    print env.

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,22 +1,42 @@
 package main
 
 import (
-	"os"
+	"fmt"
 	logging "github.com/op/go-logging"
+	"os"
+	"syscall"
 )
 
 const (
-	LOGFMT            = "%{color}%{time:15:04:05.000000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}"
+	LOGFMT = "%{color}%{time:15:04:05.000000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}"
 )
 
 //this is log file
 var (
-	logFile    *os.File
-	logFormat                = logging.MustStringFormatter(LOGFMT)
-	log                      = logging.MustGetLogger("logfile")
-	Gloglevel  logging.Level = logging.DEBUG
+	logFile   *os.File
+	logFormat               = logging.MustStringFormatter(LOGFMT)
+	log                     = logging.MustGetLogger("logfile")
+	Gloglevel logging.Level = logging.DEBUG
 )
 
 func main() {
+	log.Info("=[PATH]===================================================================")
+
+	path := os.Getenv("GOPATH")
+	fmt.Println(path)
+
+	path = os.Getenv("GOROOT")
+	fmt.Println(path)
+
+	syscall_path, ok := syscall.Getenv("_system_arch")
+	fmt.Println(syscall_path)
+	fmt.Println(ok)
+	
+	path = os.Getenv("_system_name")
+	fmt.Println(path)
+	
+	
+	
+	
 	
 }
